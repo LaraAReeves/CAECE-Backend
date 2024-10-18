094fc944ce00e6c982080dd8bd0ff8a15befa565
LaraAReeves <lareeves@estudiantes.unsam.edu.ar>
2024-10-17 21:40:43 -0300

    Se crea Repository de persona

--- src/main/java/org/unsam/entity/persona.java
+++ src/main/java/org/unsam/entity/persona.java
@@ -1,7 +1,16 @@
 package org.unsam.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "Personas")
